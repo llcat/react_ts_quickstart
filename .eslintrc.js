216742df0c1f3086502091f97b2c2db75aa52462
Pino Yu <17612709402@163.com>
2020-08-18 16:35:43 +0800

    init project with some basic config

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    es2020: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+  ],
+  rules: {
+
+  },
+};
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # react_ts_quickstart
-a react+typescript quick start scaffolds
+a react+typescript quick start scaffolds for myself project
+
+#### prettier
